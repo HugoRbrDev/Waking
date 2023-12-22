97edba4b8ab9f0995a59dcd7d74de6827f730cbd
HugoRbrDev <hugorbrdev@gmail.com>
2023-12-22 00:05:37 -0300

    Criando activity Menu

---
+++ app/src/main/java/devandroid/hugo/waking/view/MenuActivity.java
@@ -0,0 +1,16 @@
+package devandroid.hugo.waking.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import devandroid.hugo.waking.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
