ede7818ea9be529fa1cdce102a75dcd5f0c0d9e6
HugoRbrDev <hugorbrdev@gmail.com>
2024-01-11 20:22:40 -0300

    Activity financas criada

---
+++ app/src/main/java/devandroid/hugo/waking/ProfissionalActivity.java
@@ -0,0 +1,14 @@
+package devandroid.hugo.waking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfissionalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profissional);
+    }
+}
--- app/src/main/java/devandroid/hugo/waking/view/MenuActivity.java
+++ app/src/main/java/devandroid/hugo/waking/view/MenuActivity.java
@@ -1,10 +1,13 @@
 package devandroid.hugo.waking.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import devandroid.hugo.waking.FinancasActivity;
 import devandroid.hugo.waking.R;
 
 public class MenuActivity extends AppCompatActivity {
@@ -13,6 +16,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
+    }
+    //Button para a activity Finanças
+    public void buttonMenuToFinancas(View view) {
+        Intent intent = new Intent(MenuActivity.this, FinancasActivity.class);
+        startActivity(intent);
     }
 }
