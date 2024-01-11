1bff2860023463d577ac255c70da82dcce2df45d
HugoRbrDev <hugorbrdev@gmail.com>
2024-01-11 13:30:12 -0300

    Mudando o nome da classe do botão buttonMainToMenu

--- app/src/main/java/devandroid/hugo/waking/view/MainActivity.java
+++ app/src/main/java/devandroid/hugo/waking/view/MainActivity.java
@@ -10,13 +10,16 @@ import android.widget.Button;
 import devandroid.hugo.waking.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
+    //Início botão para a activity Menu
+    public void buttonMainToMenu(View view) {
+        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+        startActivity(intent);
+    }
+    //Fim botão para activity Menu
 }
